144d5cff09a3d0a667f26938eb2948a695239ae1
Brzi Gonzales <hola@gmail.com>
2018-06-05 21:41:13 +0200

    msql connection

--- index.php
+++ index.php
@@ -1,46 +1,62 @@
 
+
+  <?php include_once "connection.php" ?> 
+  <?php
+    // pripremamo upit
+    $sql = "SELECT id, title, body, author, created_at FROM posts ORDER BY created_at DESC LIMIT 3";
+    $statement = $connection->prepare($sql);
+
+    // izvrsavamo upit
+    $statement->execute();
+
+    // zelimo da se rezultat vrati kao asocijativni niz.
+    // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+    // punimo promenjivu sa rezultatom upita
+    $posts = $statement->fetchAll();
+
+    // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+        echo '<pre>';
+        var_dump($posts);
+        echo '</pre>';
+
+?>
+
+
+
+
+
 <?php include_once "partials/header.php"?>
 
 <main role="main" class="container">
 
     <div class="row">
         <div class="col-sm-8 blog-main">
-            <div class="blog-post">
-                <a  href="single-post.php"><h2 class="blog-post-title">Sample blog post</h2></a>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
             
-                <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                <hr>
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>     
-            <div class="blog-post"> 
-                <a href="#" ><h2 class="blog-post-title">Another blog post</h2></a>
-                <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+                <?php
+                foreach ($posts as $post) {
+            ?>
 
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            </div><!-- /.blog-post -->
-            <div class="blog-post">
-                <a href="#"><h2 class="blog-post-title">New feature</h2></a>
-                <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+                <h1><a href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($post['title'])?></a></h1>
 
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-            </div><!-- /.blog-post -->
+                <!-- zameniti  datum i ime sa pravim imenom i datumom blog post-a iz baze -->
+                <div class="va-c-article__meta">12. 6. 2017. by John Doe</div>
+
+                <div>
+                    <!-- zameniti ovaj privremeni (testni) text sa pravim sadrzajem blog post-a iz baze -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt vitae molestias rem
+                        repellendus commodi provident? Magnam, nobis quisquam perferendis consectetur deserunt
+                        laboriosam pariatur a, eum suscipit ratione iusto ullam aperiam quas quod culpa dolore
+                        corrupti voluptatem placeat enim commodi in.</p>
+                </div>
+
+            <?php
+                }
+            ?>    
+             <div class="va-c-paginator">
+                <a href="all-posts.php" title="All posts">All posts</a>
+            </div>
 
             <nav class="blog-pagination">
                 <a class="btn btn-outline-primary" href="#">Older</a>
--- single-post.php
+++ single-post.php
@@ -1,29 +1,32 @@
 
 <?php include_once "partials/header.php"?>
+<?php include_once "connection.php" ?> 
+<?php
+    $sql = "SELECT id, title, body, author, created_at FROM posts WHERE posts.id =" . $_GET["post_id"];
+    $statement = $connection->prepare($sql);
+
+    // izvrsavamo upit
+    $statement->execute();
+
+    // zelimo da se rezultat vrati kao asocijativni niz.
+    // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+    // punimo promenjivu sa rezultatom upita
+    $post = $statement->fetch();
+
+    // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+
+?>
 
 <main role="main" class="container">
         <div class="row">
         <div class="col-sm-8 blog-main">
-            <h2>Heading</h2>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <h3>Sub-heading</h3>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-            <pre><code>Example code block</code></pre>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-            <h3>Sub-heading</h3>
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <ul>
-                <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                <li>Donec id elit non mi porta gravida at eget metus.</li>
-                <li>Nulla vitae elit libero, a pharetra augue.</li>
-            </ul>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-            <ol>
-                <li>Vestibulum id ligula porta felis euismod semper.</li>
-                <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-            </ol>
-            <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+            <div class="blog-post">
+                <h2 class="blog-post-title"> <a href = "single-post.php?id=<?php echo($post['id']) ?>"><?php echo ($post["title"])?> </a></h2>
+                <p class="blog-post-meta"><?php echo ($post["created_at"])?> <a href="#"><?php echo ($post["author"])?></a></p>
+                <p><?php echo ($post["body"])?></p>
+            </div>
         </div>
     </div>
 </main><!-- /.container -->
